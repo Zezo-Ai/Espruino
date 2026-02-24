Zezo-Ai/Espruino

commit 870d2205112017ab65eb787c592efe4397c1b648

Fix "Not enough stack memory for data" Exception when uploading files occasionally Also updated the other uses of JSV_GET_AS_CHAR_ARRAY to either use the existing error reporting, or use a new define that doesn't create an exception

targets/nrf5x/bluetooth.c
@@ -2541,7 +2541,7 @@ static void services_init() {
 #if BLE_HIDS_ENABLED
     JsVar *hidReport = jsvObjectGetChildIfExists(execInfo.hiddenRoot, BLE_NAME_HID_DATA);
     if (hidReport) {
-      JSV_GET_AS_CHAR_ARRAY(hidPtr, hidLen, hidReport);
+      JSV_GET_AS_CHAR_ARRAY_NO_ERROR(hidPtr, hidLen, hidReport);
       if (hidPtr && hidLen) {
         hids_init((uint8_t*)hidPtr, hidLen);
         bleStatus |= BLE_HID_INITED;
@@ -2780,6 +2780,10 @@ uint32_t jsble_advertising_start() {
 
   JsVar *advDataVar = jswrap_ble_getCurrentAdvertisingData();
   JSV_GET_AS_CHAR_ARRAY(advPtr, advLen, advDataVar);
+  if (!advPtr) {
+    jsvUnLock(advDataVar);
+    return NRF_ERROR_INVALID_PARAM; // couldn't convert advertising data to char array
+  }
 
   // Set up scan response packet's contents
   ble_uuid_t adv_uuids[ADVERTISE_MAX_UUIDS];

libs/banglejs/jswrap_bangle.c
@@ -3067,6 +3067,7 @@ Writes a command directly to the ST7735 LCD controller
 */
 void jswrap_banglejs_lcdWr(JsVarInt cmd, JsVar *data) {
   JSV_GET_AS_CHAR_ARRAY(dPtr, dLen, data);
+  if (!dPtr) return;
 #ifdef LCD_CONTROLLER_ST7789_8BIT
   lcdST7789_cmd(cmd, dLen, (const uint8_t *)dPtr);
 #endif

libs/bluetooth/jswrap_bluetooth.c
@@ -1514,10 +1514,7 @@ void jswrap_ble_setScanResponse(JsVar *data) {
     jsvObjectRemoveChild(execInfo.hiddenRoot, BLE_NAME_SCAN_RESPONSE_DATA);
   } else if (jsvIsArray(data) || jsvIsArrayBuffer(data)) {
     JSV_GET_AS_CHAR_ARRAY(respPtr, respLen, data);
-    if (!respPtr) {
-      jsExceptionHere(JSET_TYPEERROR, "Unable to convert data argument to an array");
-      return;
-    }
+    if (!respPtr) return; // already errored
     // only set data if we managed to decode it ok
     jsvObjectSetOrRemoveChild(execInfo.hiddenRoot, BLE_NAME_SCAN_RESPONSE_DATA, data);
 
@@ -2677,8 +2674,7 @@ void jswrap_nfc_URL(JsVar *url) {
   }
 
   JSV_GET_AS_CHAR_ARRAY(urlPtr, urlLen, url);
-  if (!urlPtr || !urlLen)
-    return jsExceptionHere(JSET_ERROR, "Unable to get URL data");
+  if (!urlPtr) return; // already errored
 
   nfc_uri_id_t uriType = NFC_URI_NONE;
   if (memcmp(urlPtr, "http://", 7)==0) {
@@ -2740,7 +2736,7 @@ void jswrap_nfc_pair(JsVar *key) {
     return;
   }
 
-  JSV_GET_AS_CHAR_ARRAY(keyPtr, keyLen, key);
+  JSV_GET_AS_CHAR_ARRAY_NO_ERROR(keyPtr, keyLen, key);
   if (!keyPtr || keyLen!=BLE_GAP_SEC_KEY_LEN)
     return jsExceptionHere(JSET_ERROR, "Unable to get key data or key isn't 16 bytes long");
 
@@ -2788,8 +2784,7 @@ void jswrap_nfc_androidApp(JsVar *appName) {
   }
 
   JSV_GET_AS_CHAR_ARRAY(appNamePtr, appNameLen, appName);
-  if (!appNamePtr || !appNameLen)
-    return jsExceptionHere(JSET_ERROR, "Unable to get app name");
+  if (!appNamePtr) return; // already errored
 
   /* assemble NDEF Message */
   /* Encode BLE pairing message into the buffer. */
@@ -2834,7 +2829,7 @@ void jswrap_nfc_raw(JsVar *payload) {
     return;
   }
 
-  JSV_GET_AS_CHAR_ARRAY(dataPtr, dataLen, payload);
+  JSV_GET_AS_CHAR_ARRAY_NO_ERROR(dataPtr, dataLen, payload);
   if (!dataPtr || !dataLen || dataLen>NDEF_TAG2_VALUE_MAXLEN)
     return jsExceptionHere(JSET_ERROR, "Unable to get NFC data");
 
@@ -2874,10 +2869,7 @@ JsVar *jswrap_nfc_start(JsVar *payload) {
   if (!jsvIsUndefined(payload)) {
     /* Custom UID */
     JSV_GET_AS_CHAR_ARRAY(dataPtr, dataLen, payload);
-    if (!dataPtr || !dataLen) {
-      jsExceptionHere(JSET_ERROR, "Unable to get NFC data");
-      return 0;
-    }
+    if (!dataPtr) return 0; // already errored
     flatStr = jsvNewFlatStringOfLength(dataLen);
     if (!flatStr) {
       jsExceptionHere(JSET_ERROR, "Unable to create string with NFC data in");
@@ -2975,8 +2967,7 @@ void jswrap_nfc_send(JsVar *payload) {
 
   /* Send n byte payload */
   JSV_GET_AS_CHAR_ARRAY(dataPtr, dataLen, payload);
-  if (!dataPtr || !dataLen)
-    return jsExceptionHere(JSET_ERROR, "Unable to get NFC data");
+  if (!dataPtr) return; // already errored
 
   jsble_nfc_send((uint8_t*)dataPtr, dataLen);
 #endif
@@ -3005,9 +2996,7 @@ void jswrap_ble_sendHIDReport(JsVar *data, JsVar *callback) {
     if (jsvIsFunction(callback))
       jsvObjectSetChild(execInfo.root, BLE_HID_SENT_EVENT, callback);
     jsble_send_hid_input_report((uint8_t*)vPtr, vLen);
-  } else {
-    jsExceptionHere(JSET_ERROR, "Expecting Array, got %t", data);
-  }
+  } // already errored
 #endif
 }
 
@@ -4775,7 +4764,7 @@ NRF.connect(device_address).then(function(d) {
 JsVar *jswrap_ble_BluetoothRemoteGATTCharacteristic_writeValue(JsVar *characteristic, JsVar *data) {
 #if CENTRAL_LINK_COUNT>0
   JSV_GET_AS_CHAR_ARRAY(dataPtr, dataLen, data);
-  if (!dataPtr) return 0;
+  if (!dataPtr) return 0; // already errored
 
   if (!bleNewTask(BLETASK_CHARACTERISTIC_WRITE, 0))
     return 0;

libs/crypto/jswrap_crypto.c
@@ -159,7 +159,7 @@ JsVar *jswrap_crypto_SHAx(JsVar *message, int shaNum) {
 #endif
 
   JSV_GET_AS_CHAR_ARRAY(msgPtr, msgLen, message);
-  if (!msgPtr) return 0;
+  if (!msgPtr) return 0; // already errored
 
   int bufferSize = 20;
   if (shaNum>1) bufferSize = shaNum/8;
@@ -307,9 +307,9 @@ JsVar *jswrap_crypto_PBKDF2(JsVar *passphrase, JsVar *salt, JsVar *options) {
     return 0; // already shown an error
 
   JSV_GET_AS_CHAR_ARRAY(passPtr, passLen, passphrase);
-  if (!passPtr) return 0;
+  if (!passPtr) return 0; // already errored
   JSV_GET_AS_CHAR_ARRAY(saltPtr, saltLen, salt);
-  if (!saltPtr) return 0;
+  if (!saltPtr) return 0; // already errored
 
   int err;
   mbedtls_md_context_t ctx;
@@ -372,10 +372,10 @@ static NO_INLINE JsVar *jswrap_crypto_AEScrypt(JsVar *message, JsVar *key, JsVar
   mbedtls_aes_init( &aes );
 
   JSV_GET_AS_CHAR_ARRAY(messagePtr, messageLen, message);
-  if (!messagePtr) return 0;
+  if (!messagePtr) return 0; // already errored
 
   JSV_GET_AS_CHAR_ARRAY(keyPtr, keyLen, key);
-  if (!keyPtr) return 0;
+  if (!keyPtr) return 0; // already errored
 
   if (encrypt)
     err = mbedtls_aes_setkey_enc( &aes, (unsigned char*)keyPtr, (unsigned int)keyLen*8 );
@@ -512,10 +512,10 @@ JsVar *jswrap_crypto_AES_ccmCrypt(JsVar *message, JsVar *key, JsVar *iv, JsVar *
   }
 
   JSV_GET_AS_CHAR_ARRAY(messagePtr, messageLen, message);
-  if (!messagePtr) return NULL;
+  if (!messagePtr) return NULL; // already errored
 
   JSV_GET_AS_CHAR_ARRAY(keyPtr, keyLen, key);
-  if (!keyPtr) return NULL;
+  if (!keyPtr) return NULL; // already errored
 
   char *outMessagePtr = NULL;
   JsVar *outVar = jsvNewArrayBufferWithPtr(messageLen, &outMessagePtr);

libs/graphics/jswrap_graphics.c
@@ -5051,8 +5051,8 @@ JsVar *jswrap_graphics_filter(JsVar *parent, JsVar *filter, JsVar *options) {
     {"filter", JSV_STRING_0, &filtVar},
   };
   JSV_GET_AS_CHAR_ARRAY(filtPtr, filtLen, filter);
-  if (!jsvReadConfigObject(options, configs, sizeof(configs) / sizeof(jsvConfigObject))
-      || !filtPtr) {
+  if (!filtPtr) return 0; // already errored
+  if (!jsvReadConfigObject(options, configs, sizeof(configs) / sizeof(jsvConfigObject))) {
     jsExceptionHere(JSET_ERROR, "Invalid options");
     return 0;
   }

libs/hexbadge/jswrap_hexbadge.c
@@ -224,7 +224,7 @@ void badge_lcd_flip(JsVar *g) {
   JsVar *buf = jsvObjectGetChildIfExists(g,"buffer");
   if (!buf) return;
   JSV_GET_AS_CHAR_ARRAY(bPtr, bLen, buf);
-  if (!bPtr || bLen<128*8) return;
+  if (!bPtr || bLen<128*8) return; // already errored
 
   jshPinSetValue(LCD_CS,0);
   for (int y=0;y<8;y++) {
@@ -302,7 +302,7 @@ void jswrap_badge_init() {
     15, 11, 15, 15, 11, 15, 31, 22, 30, 30, 22, 30, 30, 8, 8, 12, 15, 11, 15, 3, 1
   };
   JsVar *buf = jsvObjectGetChildIfExists(graphics,"buffer");
-  JSV_GET_AS_CHAR_ARRAY(bPtr, bLen, buf);
+  JSV_GET_AS_CHAR_ARRAY_NO_ERROR(bPtr, bLen, buf);
   if (bPtr) memcpy(&bPtr[LCD_IMIT_IMG_OFFSET], LCD_INIT_IMG, sizeof(LCD_INIT_IMG));
   jsvUnLock(buf);
   // Create 'flip' fn

libs/neopixel/jswrap_neopixel.c
@@ -111,10 +111,7 @@ range.
 */
 void jswrap_neopixel_write(Pin pin, JsVar *data) {
   JSV_GET_AS_CHAR_ARRAY(rgbData, rgbSize, data);
-  if (!rgbData) {
-    jsExceptionHere(JSET_ERROR, "Couldn't convert %t to data to send to LEDs", data);
-    return;
-  }
+  if (!rgbData) return; // already errored
   if (rgbSize == 0) {
     jsExceptionHere(JSET_ERROR, "Data must be a non empty array");
     return;

libs/network/network.c
@@ -378,6 +378,10 @@ JsVar *load_cert_file(JsVar *cert) {
   }
 
   JSV_GET_AS_CHAR_ARRAY(pPtr, pLen, fileContents);
+  if (!pPtr) {
+    jsvUnLock(fileContents);
+    return 0; // already errored
+  }
   bool started = false;
   unsigned int i = 0;
   unsigned int j = 0;

commit b627cb9496baf5fc37cc330690e91d1bc0a017b2

Fix Bluetooth regression after 870d2205112017ab65eb787c592efe4397c1b648 (BLE devices don't advertise at first boot)

targets/nrf5x/bluetooth.c
@@ -2785,10 +2785,7 @@ uint32_t jsble_advertising_start() {
 
   JsVar *advDataVar = jswrap_ble_getCurrentAdvertisingData();
   JSV_GET_AS_CHAR_ARRAY(advPtr, advLen, advDataVar);
-  if (!advPtr) {
-    jsvUnLock(advDataVar);
-    return NRF_ERROR_INVALID_PARAM; // couldn't convert advertising data to char array
-  }
+  // advPtr can be 0 here (esp when booting as we have no adv data)
 
   // Set up scan response packet's contents
   ble_uuid_t adv_uuids[ADVERTISE_MAX_UUIDS];
@@ -2904,7 +2901,7 @@ uint32_t jsble_advertising_start() {
   memset(&m_ble_gap_adv_data, 0, sizeof(m_ble_gap_adv_data));
 #endif
   err_code = jsble_advertising_update(
-    (uint8_t*)advPtr, advLen,
+    (uint8_t*)advPtr, advLen, // advPtr can be NULL here and it's no big deal
     non_scannable ? NULL : m_enc_scan_response_data, non_scannable ? 0 : m_enc_scan_response_data_len,
     &adv_params
   );